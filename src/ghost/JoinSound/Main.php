493cd77dc0275835d2388a87fab40b57ca2d41eb
Ghost97400 <77570455+Ghost97400@users.noreply.github.com>
2021-12-27 09:15:19 +0200

    Command Break

---
+++ src/ghost/JoinSound/Main.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace ghost\JoinSound;
+
+use pocketmine\math\Vector3;
+use pocketmine\player\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\world\sound\BlazeShootSound;
+
+class main extends PluginBase implements Listener {
+
+    public function onEnable(): void{
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    public function onCommand(CommandSender $sender, Command $command, string $label, Array $args) : bool {
+
+        switch($command->getName()){
+            case "join sound":
+                if ($sender instanceof Player){
+                    $sender->sendMessage("plugin fais par my name is6758");
+                }
+        }
+    return true;
+    }
+
+    public function onJoin(PlayerJoinEvent $event){
+        $player = $event->getPlayer();
+        $world = $player->getWorld();
+        $x = $player->getPosition()->getX();
+        $y = $player->getPosition()->gety();
+        $z = $player->getPosition()->getz();
+
+
+        $world->addSound(new Vector3($x, $y, $z), new BlazeShootSound($player));
+    }
+}
